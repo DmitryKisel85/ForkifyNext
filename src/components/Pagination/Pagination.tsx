c7716cc64f706369cad6df3df1c134f9d9c5c0c7
Dmitry Kisel <dim03@mail.ru>
2022-10-26 21:29:46 +0300

    Add Pagination: add components and its logic and styles

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,7 +1,88 @@
-import React from "react";
+import classnames from "classnames";
+import { usePagination, DOTS } from "../../hooks/usePagination";
 
-const Pagination = () => {
-    return <div>Pagination</div>;
+import styles from "./pagination.module.scss";
+
+type PaginationProps = {
+    onPageChange: (currentPage: number) => void;
+    totalCount: number;
+    siblingCount?: number;
+    currentPage: number;
+    pageSize: number;
+};
+
+const Pagination = (props: PaginationProps) => {
+    const { onPageChange, totalCount, siblingCount = 1, currentPage, pageSize } = props;
+
+    const paginationRange = usePagination({
+        currentPage,
+        totalCount,
+        siblingCount,
+        pageSize,
+    });
+
+    // If there are less than 2 times in pagination range we shall not render the component
+    if (currentPage === 0 || (paginationRange && paginationRange.length < 2)) {
+        return null;
+    }
+
+    const onNext = () => {
+        onPageChange(currentPage + 1);
+    };
+
+    const onPrevious = () => {
+        onPageChange(currentPage - 1);
+    };
+
+    let lastPage = paginationRange && paginationRange[paginationRange.length - 1];
+    return (
+        <ul className={classnames(styles.paginationContainer, styles.paginationBar)}>
+            {/* Left navigation arrow */}
+            <li
+                className={classnames(styles.paginationItem, {
+                    [styles.paginationItemDisabled]: currentPage === 1,
+                })}
+                onClick={onPrevious}
+            >
+                <div className={classnames(styles.arrow, styles.arrowLeft)} />
+            </li>
+            {paginationRange &&
+                paginationRange.map((pageNumber, index) => {
+                    // If the pageItem is a DOT, render the DOTS unicode character
+                    if (pageNumber === DOTS) {
+                        return (
+                            <li key={index} className={styles.paginationItemDots}>
+                                &#8230;
+                            </li>
+                        );
+                    }
+                    if (typeof pageNumber === "number") {
+                        return (
+                            <li
+                                key={index}
+                                className={classnames(styles.paginationItem, {
+                                    [styles.paginationItemSelected]: pageNumber === currentPage,
+                                })}
+                                onClick={() => onPageChange(pageNumber)}
+                            >
+                                {pageNumber}
+                            </li>
+                        );
+                    }
+
+                    // Render our Page Pills
+                })}
+            {/*  Right Navigation arrow */}
+            <li
+                className={classnames(styles.paginationItem, {
+                    [styles.paginationItemDisabled]: currentPage === lastPage,
+                })}
+                onClick={onNext}
+            >
+                <div className={classnames(styles.arrow, styles.arrowRight)} />
+            </li>
+        </ul>
+    );
 };
 
 export default Pagination;
--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,15 +1,76 @@
 import Results from "../Results";
 import Pagination from "../Pagination";
 
+import { useGetMealsQuery } from "../../services/ForkifyServices";
+import { useAppSelector, useAppDispatch } from "../../hooks/typedHooks";
+
+import { getRecipeId } from "../../store/recipe/recipeSlice";
+
+import { searchTermSelector } from "../../store/search/searchSelector";
+
+import ResultsPreviewElement from "../ResultsPreviewElement";
+import ErrorMessage from "../ErrorMessage";
+import Spinner from "../Spinner";
+
+import { useMemo, useState } from "react";
+
 import styles from "./searchResults.module.scss";
 
+let PageSize = 10;
+
 const SearchResults = () => {
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [activeElement, setActiveElement] = useState<string | null>(null);
+    const searchTerm = useAppSelector(searchTermSelector);
+
+    const dispatch = useAppDispatch();
+
+    const { data, isLoading, isError } = useGetMealsQuery(searchTerm);
+
+    const result = data?.data.recipes;
+    const lengthTotal = result?.length;
+    // const lengthTotal = data && data?.data.recipes.length;
+
+    const updateActiveElement = (id: string) => {
+        setActiveElement(id);
+        dispatch(getRecipeId(id));
+    };
+
+    const currentTableData = useMemo(() => {
+        const firstPageIndex = (currentPage - 1) * PageSize;
+        const lastPageIndex = firstPageIndex + PageSize;
+        return result?.slice(firstPageIndex, lastPageIndex);
+    }, [currentPage, result]);
+
+    if (!searchTerm) return <div></div>;
+    if (isLoading) return <Spinner />;
+    if (isError) return <ErrorMessage message="Something wrong has happened! Please, refresh the page!" />;
+
     return (
         <div className={styles.searchResults}>
-            <Results />
-            <div className={styles.pagination}>
-                <Pagination />
-            </div>
+            {data && data.results > 0 ? (
+                <ul className={styles.results}>
+                    {currentTableData?.map((meal) => {
+                        return (
+                            <ResultsPreviewElement
+                                key={meal.id}
+                                meal={meal}
+                                onClick={() => updateActiveElement(meal.id)}
+                                activeElement={activeElement}
+                            />
+                        );
+                    })}
+                </ul>
+            ) : (
+                <ErrorMessage message="We could not find that recipe. Please try another one!" />
+            )}
+
+            <Pagination
+                currentPage={currentPage}
+                totalCount={lengthTotal || 0}
+                pageSize={PageSize}
+                onPageChange={(page) => setCurrentPage(page)}
+            />
 
             <p className={styles.copyright}>&copy; Design by Jonas Schmedtmann.</p>
         </div>
