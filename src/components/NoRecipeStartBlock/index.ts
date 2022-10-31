b48f34894c755a6a0c7fc944b67523fe0f77bfd1
Dmitry Kisel <dim03@mail.ru>
2022-10-31 17:36:00 +0300

    Refactor app: delete unused NoRecipeStartBlock

--- src/components/NoRecipeStartBlock/index.ts
+++
@@ -1,3 +0,0 @@
-import "./NoRecipeStartBlock";
-
-export { default } from "./NoRecipeStartBlock";
--- src/components/Recipe/Recipe.tsx
+++ src/components/Recipe/Recipe.tsx
@@ -1,10 +1,12 @@
-import NoRecipeStartBlock from "../NoRecipeStartBlock";
 import RecipeElement from "../RecipeElement";
+
+import { FaRegSmileBeam } from "react-icons/fa";
 
 import { recipeIdSelector } from "../../store/recipe/recipeSelector";
 import { useAppSelector } from "../../hooks/typedHooks";
 
 import styles from "./recipe.module.scss";
+import RenderMessage from "../RenderMessage";
 
 const Recipe = () => {
     const chosenRecipeId = useAppSelector(recipeIdSelector);
@@ -14,7 +16,10 @@ const Recipe = () => {
             {chosenRecipeId ? (
                 <RecipeElement id={chosenRecipeId} />
             ) : (
-                <NoRecipeStartBlock />
+                <RenderMessage
+                    messageText="Start by searching for a recipe or an ingredient. Have fun!"
+                    messageIcon={<FaRegSmileBeam />}
+                />
             )}
         </div>
     );
