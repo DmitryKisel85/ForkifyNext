900a9a4f58fd2fd02d0056b29d8350857f65315f
Dmitry Kisel <dim03@mail.ru>
2022-11-01 16:54:39 +0300

    Refactor ResultsPreviewElement: fix markup and delete unused styles

--- src/components/ResultsPreviewElement/ResultsPreviewElement.tsx
+++ src/components/ResultsPreviewElement/ResultsPreviewElement.tsx
@@ -10,33 +10,18 @@ type ResultsPreviewElementProps = {
     activeElement?: string | null;
 };
 
-const ResultsPreviewElement = ({
-    meal,
-    onClick,
-    activeElement,
-}: ResultsPreviewElementProps) => {
+const ResultsPreviewElement = ({ meal, onClick, activeElement }: ResultsPreviewElementProps) => {
     return (
         <li key={meal.id} className={styles.preview} onClick={onClick}>
-            <a
-                className={classNames(
-                    styles.previewLink,
-                    activeElement === meal.id && styles.previewLinkActive
-                )}
-                href={`#${meal.id}`}
-            >
+            <div className={classNames(styles.previewLink, activeElement === meal.id && styles.previewLinkActive)}>
                 <figure className={styles.previewFig}>
                     <img src={meal.image_url} alt={meal.title} />
                 </figure>
                 <div className={styles.previewData}>
                     <h4 className={styles.previewTitle}>{meal.title}</h4>
                     <p className={styles.previewPublisher}>{meal.publisher}</p>
-                    {/* <div className="preview__user-generated ${this._data.key ? '' : 'hidden'}">
-                        <svg>
-                            <use href="${icons}#icon-user"></use>
-                        </svg>
-                    </div> */}
                 </div>
-            </a>
+            </div>
         </li>
     );
 };
